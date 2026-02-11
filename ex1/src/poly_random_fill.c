21389bcbc6f61de4cc0706ae2b79b9662e02eec0
costas-g <210527419+costas-g@users.noreply.github.com>
2026-02-11 03:59:46 +0200

    Added option to control the signedness of the coefficients.

--- ex1/src/poly_random_fill.c
+++ ex1/src/poly_random_fill.c
@@ -1,17 +1,26 @@
 #include <stdlib.h>
 
+#define POS_COEFFS
+
 void poly_random_fill(int * const poly_out, const size_t degree, int max_coeff) {
     /* compute final max coefficient */
     if (max_coeff < 1 || max_coeff > RAND_MAX/2) max_coeff = RAND_MAX;
-    // else max_coeff = 2*max_coeff;
+    #ifndef POS_COEFFS
+    else max_coeff = 2*max_coeff;
+    #endif
 
     /* Random fill with non zeroes */
     for (size_t i = 0; i <= degree; i++){
         int coeff;
-        // do {
-            coeff = rand() % max_coeff + 1;// - max_coeff/2;   /* in range [-max_coeff, max_coeff-1] */
-        // }
-        // while (coeff == 0); /* coefficients should be non-zero integers */
+
+        #ifdef POS_COEFFS
+        coeff = rand() % max_coeff + 1; /* in range [1, max_coeff] */
+        #else
+        do {
+            coeff = rand() % max_coeff - max_coeff/2;   /* in range [-max_coeff, max_coeff-1] */
+        }
+        while (coeff == 0); /* coefficients should be non-zero integers */
+        #endif
 
         poly_out[i] = (int) coeff; /* assign value */
     }
